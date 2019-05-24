f80818eb3f25514fe9f630961ad7d95467d96a9b
LolaEnBeta <lolitaenbeta@gmail.com>
2019-05-24 17:03:15 +0200

    Create a route that returns a list of users

--- api-rest-ex.py
+++ api-rest-ex.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask (__name__)
+
+users = [
+    {"userName": "User1",
+    "id": 1,
+    "email": "user1@hi.com",
+    "state": False},
+
+    {"userName": "User2",
+    "id": 2,
+    "email": "user2@hi.com",
+    "state": False},
+
+    {"userName": "User3",
+    "id": 3,
+    "email": "user3@hi.com",
+    "state": False},
+]
+
 
 @app.route("/")
 def index():
     return "Hello world!"
+
+@app.route("/users", methods=["GET"])
+def get_users():
+    return jsonify({"List of users": users})
 
 if __name__ == "__main__":
     app.run(debug=True)
